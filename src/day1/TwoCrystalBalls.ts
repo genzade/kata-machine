b78bd60697ac6095c1e3286d7f8bcc00b620c87c
Hamid Zadeh <hamidmamadzade@gmail.com>
2024-03-28 16:51:35 +0000

    Implemented two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumps = Math.floor(Math.sqrt(breaks.length));
 
+    let lo = 0;
+    let hi = breaks.length;
+
+    for (let i = 0; i < breaks.length; i += jumps) {
+        if (breaks[i] === false) {
+            lo = i;
+        } else {
+            hi = i;
+
+            for (let j = lo + 1; j < hi; j++) {
+                if (breaks[j] === true) return j;
+            }
+        }
+    }
+
+    return -1;
 }
